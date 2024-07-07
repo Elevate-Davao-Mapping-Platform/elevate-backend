2209f7809871ba7c331be57cf792f78e6c9a784f
ArJSarmiento <rneljan@gmail.com>
2024-07-07 17:26:16 +0800

    Initial commit

---
+++ tests/unit/test_elevate_be_stack.py
@@ -0,0 +1,15 @@
+import aws_cdk as core
+import aws_cdk.assertions as assertions
+
+from elevate_be.elevate_be_stack import ElevateBeStack
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in elevate_be/elevate_be_stack.py
+def test_sqs_queue_created():
+    app = core.App()
+    stack = ElevateBeStack(app, "elevate-be")
+    template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
