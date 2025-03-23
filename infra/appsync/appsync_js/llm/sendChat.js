c2248c1d5b5eb16616554a9b7d576d0acd964e37
Arnel Jan Sarmiento <arnel@advicerevolution.com.au>
2025-03-23 21:06:06 +0800

    KAN-37 enhance sendChat functionality and update GraphQL schema

--- infra/appsync/appsync_js/llm/sendChat.js
+++ infra/appsync/appsync_js/llm/sendChat.js
@@ -2,12 +2,17 @@ import { util } from "@aws-appsync/utils";
 
 export function request(ctx) {
     const entryId = util.autoId();
+    const chatTopicId = ctx.args.input.chatTopicId || util.autoId();
     const arguments_payload = {
         ...ctx.args.input,
         entryId,
-    };
+    }
+    if (!ctx.args.input.chatTopicId) {
+        arguments_payload.chatTopicId = chatTopicId;
+    }
 
     ctx.stash.entryId = entryId;
+    ctx.stash.chatTopicId = chatTopicId;
 
     return {
         operation: "Invoke",
@@ -18,8 +23,9 @@ export function request(ctx) {
 
 export function response(ctx) {
     return {
-        id: ctx.stash.entryId,
+        entryId: ctx.stash.entryId,
         message: "Chat sent successfully",
         success: true,
+        chatTopicId: ctx.stash.chatTopicId,
     };
 }
--- src/rag_api/usecases/chat_usecase.py
+++ src/rag_api/usecases/chat_usecase.py
@@ -16,27 +16,30 @@ class ChatUsecase:
         self.llm_usecase = LLMUsecase()
         self.logger = Logger()
 
-    def process_chat(self, chat_in: ChatPromptIn):
+    def process_chat(self, chat_prompt_in: ChatPromptIn):
         """
         Process a user prompt and generate a response using a language model.
 
-        :param ChatPromptIn chat_in: The user prompt to generate a response for.
+        :param ChatPromptIn chat_prompt_in: The user prompt to generate a response for.
         :return dict: A dictionary containing the response and the status code.
         """
         status = HTTPStatus.OK
+        chat_history = []
 
         # Create Chat Topic
-        chat_topic_id = chat_in.chatTopicId
+        chat_topic_id = chat_prompt_in.chatTopicId
+        query_chat_topic_status = HTTPStatus.OK
         if chat_topic_id:
-            status, _, message = self.chat_topic_repository.query_chat_topic(
-                chat_in.userId, chat_topic_id
+            query_chat_topic_status, _, _ = self.chat_topic_repository.query_chat_topic(
+                chat_prompt_in.userId, chat_topic_id
             )
 
-        if not chat_topic_id or status != HTTPStatus.OK:
+        if not chat_topic_id or query_chat_topic_status != HTTPStatus.OK:
             status, chat_topic, message = self.chat_topic_repository.store_chat_topic(
                 chat_topic_in=ChatTopicIn(
-                    userId=chat_in.userId,
-                    title=chat_in.query,
+                    userId=chat_prompt_in.userId,
+                    title=chat_prompt_in.query,
+                    entryId=chat_topic_id,
                 )
             )
             if status != HTTPStatus.OK:
@@ -46,24 +49,22 @@ class ChatUsecase:
                 }
 
             chat_topic_id = chat_topic.entryId
-            chat_in.chatTopicId = chat_topic_id
+            chat_prompt_in.chatTopicId = chat_topic_id
 
         else:
-            _, chats, _ = self.chat_repository.get_chats_in_topic(chat_topic_id, chat_in.userId)
+            _, chats, _ = self.chat_repository.get_chats_in_topic(
+                chat_topic_id, chat_prompt_in.userId
+            )
+            chats_sorted = sorted(chats, key=lambda x: x.createDate, reverse=True)
+            chat_history = [chat.message for chat in chats_sorted]
 
-        chats = chats or []
-        chats = sorted(chats, key=lambda x: x.createDate, reverse=True)
-        chat_history_for_prompt = '\n'.join([chat.message for chat in chats])
-
-        llm_response = self.llm_usecase.generate_response(chat_in, chat_history_for_prompt)
-
-        # Store the query chat
+        # Store the prompt chat
         user_prompt_chat_in = ChatIn(
-            userId=chat_in.userId,
+            userId=chat_prompt_in.userId,
             chatTopicId=chat_topic_id,
-            message=chat_in.query,
+            message=chat_prompt_in.query,
             type=ChatType.USER_PROMPT.value,
-            entryId=chat_in.entryId,
+            entryId=chat_prompt_in.entryId,
         )
         status, user_prompt_chat, message = self.chat_repository.store_chat(
             chat_in=user_prompt_chat_in,
@@ -74,9 +75,12 @@ class ChatUsecase:
                 'status': status,
             }
 
+        chat_history_for_prompt = '\n'.join(chat_history) if chat_history else 'No Chat History'
+        llm_response = self.llm_usecase.generate_response(chat_prompt_in, chat_history_for_prompt)
+
         # Store the response chat
         llm_response_chat_in = ChatIn(
-            userId=chat_in.userId,
+            userId=chat_prompt_in.userId,
             chatTopicId=chat_topic_id,
             message=llm_response,
             type=ChatType.LLM_RESPONSE.value,
@@ -93,7 +97,7 @@ class ChatUsecase:
         chat_data = ChatOut(
             response=llm_response,
             chatTopicId=chat_topic_id,
-            userId=chat_in.userId,
+            userId=chat_prompt_in.userId,
             entryId=user_prompt_chat.entryId,
         )
         return chat_data.model_dump()
