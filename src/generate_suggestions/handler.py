ed5898a0850a877580d1743d97389a61f6b54b9c
Arnel Jan Sarmiento <arnel@advicerevolution.com.au>
2025-04-22 22:24:33 +0800

    KAN-90: Enhance suggestion retrieval by integrating suggestions cron job and refining entity selection logic for improved match generation

--- src/generate_suggestions/handler.py
+++ src/generate_suggestions/handler.py
@@ -1,5 +1,4 @@
 from aws_lambda_powertools import Logger
-from aws_lambda_powertools.utilities.data_classes import EventBridgeEvent, event_source
 from aws_lambda_powertools.utilities.typing import LambdaContext
 from generate_suggestions.controllers.suggestions_controller import (
     SuggestionsController,
@@ -9,9 +8,11 @@ logger = Logger()
 
 
 @logger.inject_lambda_context
-@event_source(data_class=EventBridgeEvent)
-def handler(event: EventBridgeEvent, context: LambdaContext) -> dict:
+def handler(event: dict, context: LambdaContext) -> dict:
     _, _ = event, context
 
+    entity_id = event.get('prev', {}).get('result', {}).get('id')
+    entity_id_list = [entity_id] if entity_id else None
+
     suggestions_controller = SuggestionsController()
-    return suggestions_controller.get_suggestions()
+    return suggestions_controller.get_suggestions(entity_id_list)
