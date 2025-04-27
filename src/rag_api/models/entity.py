c0949984c8ad54f5e504447e8041a225cdfb1cff
Kaye Jose <josekayed@gmail.com>
2025-04-28 00:53:44 +0800

    fix: use BooleanAttribute

--- src/rag_api/models/entity.py
+++ src/rag_api/models/entity.py
@@ -1,6 +1,6 @@
 import os
 
-from pynamodb.attributes import DiscriminatorAttribute, UnicodeAttribute
+from pynamodb.attributes import DiscriminatorAttribute, UnicodeAttribute, BooleanAttribute
 from pynamodb.models import Model
 
 
